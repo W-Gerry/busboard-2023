e2b1fa242ac5ea8b4b87a0a83ec5c7652e15fc2e
Will Gerry <william.gerry@outlook.com>
2023-07-07 12:09:03 +0100

    "Second commit"

--- busBoard.mjs
+++ busBoard.mjs
@@ -1,21 +1,41 @@
 // Import fetch module to use with API
-// Acts asynchronously so code to use this must be in a function
 import fetch from 'node-fetch';
+import * as readline_sync from "readline-sync";
+
 
 
 // Define key variables for retrieving bus data
 const stopCode = "490008660N";
+const userPostcode = "SE7 7FH"
 
 
 // Main function for displaying BusBoard
 // async denotes that this function contains code which can be run in the background
-async function busBoard(arrivalData) {
+async function busBoard() {
+    const coordData = await retrievePostcodeData(userPostcode);
+    displayLongLat(coordData, userPostcode);
+
     const arrivalPredictions = await getArrivals(stopCode);
     displayArrivals(arrivalPredictions);
 }
 
 
-// Function to retrieve buss arrival data 
+// Function to retrieve long and lat from postcode
+async function retrievePostcodeData(userPostcode) {
+    const postcodeResponse = await fetch(`https://api.postcodes.io/postcodes/${userPostcode}`);
+    const postcodeData = await postcodeResponse.json();
+
+    return postcodeData;
+}
+
+
+// Function to display the coordinate data
+function displayLongLat(coordData, userPostcode) {
+    console.log(`The longitude of your postcode (${userPostcode}) is ${coordData.result.longitude} and the latitude is ${coordData.result.latitude}`);
+}
+
+
+// Function to retrieve bus arrival data 
 // async denotes that this function contains code which can be run in the background
 async function getArrivals(stopCode) {
     const arrivalsResponse = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals`)
