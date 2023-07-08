e5ceaa5b0a187c2b900b1cad8641f99c9c4f5184
Will Gerry <william.gerry@outlook.com>
2023-07-08 16:00:53 +0100

    Added function to retrieve local stop points

--- busBoard.mjs
+++ busBoard.mjs
@@ -5,8 +5,8 @@ import * as readline_sync from "readline-sync";
 
 
 // Define key variables for retrieving bus data
-const stopCode = "490008660N";
-const userPostcode = "SE7 7FH"
+const userPostcode = readline_sync.question("Please enter your Postcode: ");
+const radius = readline_sync.question("Please enter a search radius in 'm': ")
 
 
 // Main function for displaying BusBoard
@@ -15,8 +15,12 @@ async function busBoard() {
     const coordData = await retrievePostcodeData(userPostcode);
     displayLongLat(coordData, userPostcode);
 
-    const arrivalPredictions = await getArrivals(stopCode);
-    displayArrivals(arrivalPredictions);
+    const localStopPoints = await retrieveStopPoints(coordData, radius);
+
+    // const arrivalPredictions = await getArrivals(stopCode);
+    // arrivalPredictions.sort((predictionA, predictionB) => predictionA.timeToStation - predictionB.timeToStation);
+
+    // displayArrivals(arrivalPredictions);
 }
 
 
@@ -35,23 +39,32 @@ function displayLongLat(coordData, userPostcode) {
 }
 
 
-// Function to retrieve bus arrival data 
-// async denotes that this function contains code which can be run in the background
-async function getArrivals(stopCode) {
-    const arrivalsResponse = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals`)
-    const arrivalData = await arrivalsResponse.json();
+// Function to get StopPoints in radius 1000m from lat and long data
+async function retrieveStopPoints(coordData, radius) {
+    const stopPointsResponse = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${coordData.result.latitude}&lon=${coordData.result.longitude}&stopTypes=NaptanOnstreetBusCoachStopPair&radius=${radius}`);
+    const stopPoints = await stopPointsResponse.json();
 
-    return arrivalData;
+    return stopPoints;
 }
 
 
-// Function to print bus data
-function displayArrivals(arrivalPredictions) {
-    for (let i = 0; i < arrivalPredictions.length; i++) {
-        const prediction = arrivalPredictions[i];
-        console.log(`\nBus number ${prediction.lineName} to ${prediction.destinationName} will arrive in approximately ${Math.floor(prediction.timeToStation/60)} minutes.`);
-    }
-}
+// // Function to retrieve bus arrival data 
+// // async denotes that this function contains code which can be run in the background
+// async function getArrivals(stopCode) {
+//     const arrivalsResponse = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals`)
+//     const arrivalData = await arrivalsResponse.json();
+
+//     return arrivalData;
+// }
+
+
+// // Function to print bus data
+// function displayArrivals(arrivalPredictions) {
+//     for (let i = 0; i < arrivalPredictions.length; i++) {
+//         const prediction = arrivalPredictions[i];
+//         console.log(`\nBus number ${prediction.lineName} to ${prediction.destinationName} will arrive in approximately ${Math.floor(prediction.timeToStation/60)} minutes.`);
+//     }
+// }
 
 
 // Run the main function
